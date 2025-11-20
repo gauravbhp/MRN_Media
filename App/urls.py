ee54b27bcf618f4c7e2a9154e15726387e6b58d8
Gaurav Singh Bhadauriya <153903427+gauravbhp@users.noreply.github.com>
2025-11-20 17:15:35 +0530

    Add download route and static file serving

--- App/urls.py
+++ App/urls.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from App import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.mrn_form, name='mrn_form'),
+    path('download/', views.download_file, name='download_file'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
